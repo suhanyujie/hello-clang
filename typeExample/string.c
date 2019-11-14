b14d4ccc2b3f45fa77ce8357e9bab8905257fe74
suhanyujie <suhanyujie@qq.com>
2019-11-14 09:17:58 +0800

    add string example

---
+++ typeExample/string.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ -----------------
+ | 函数声明区
+ -----------------
+*/
+void string_declearation();
+void example_strcpy();
+
+int main(int argc, char const *argv[])
+{
+    printf("initial: hello world!\n");
+    // string_declearation();
+    example_strcpy();
+
+
+    return 0;
+}
+
+/*
+ -----------------
+ | 函数实现区
+ -----------------
+*/
+// 字符串声明
+void string_declearation()
+{
+    // 声明方式 1
+    char str[] = "hello world";
+    printf("%s\n", str);
+    // 声明方式 2
+    char str2[12] = {'h', 'e', 'l', 'l', 'o', '\0'};
+    printf("%s\n", str2);
+    // 可以不需要把 null 字符（'\0'）放在字符串常量的末尾。C 编译器会在初始化数组时，自动把 '\0' 放在字符串的末尾。
+    // 因此在声明字符串时，需要额外的一个字符空间存放 null 字符
+    char str2_1[12] = {'h', 'e', 'l', 'l', 'o'};
+    printf("%s\n", str2_1);
+}
+
+// strcpy 示例
+void example_strcpy()
+{
+    char str1[20];
+    char str2[20] = "this is a repo";
+    printf("%s\n", str1);
+    strcpy(str1, str2);
+    printf("%s\n", str1);
+}
+
+
+/*
+ -----------------
+ | 笔记区
+ -----------------
+*/
+/*
+
+# 字符串
+* c 语言中，字符串实际上是一维数组，以字符 '\0' 结尾的一维数组。这里的 '\0' 又被称为 null 字符。
+
+
+## 字符串操作函数
+* 通过引入 `<string.h>` 头文件，我们可以调用很多字符串处理的函数，例如 `strcpy`，`strcat`，`strlen` 等
+
+ */
