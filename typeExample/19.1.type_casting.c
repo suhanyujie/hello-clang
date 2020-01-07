525381383efcb68c50985679fa0ac4137e6d3c65
suhanyujie <suhanyujie@qq.com>
2020-01-07 08:53:56 +0800

    整数提升

--- typeExample/19.1.type_casting.c
+++ typeExample/19.1.type_casting.c
@@ -7,11 +7,13 @@
  -----------------
 */
 void example_for_simple_transfer();
+void example_for_integer_imporve_transfer();
 
 int main(int argc, char const *argv[])
 {
     printf("initial print:hello world!\n");
-    example_for_simple_transfer();
+    //example_for_simple_transfer();
+    example_for_integer_imporve_transfer();
 
     return 0;
 }
@@ -27,6 +29,16 @@ void example_for_simple_transfer()
     double average;
     average = (double)sum / count;
     printf("%.2f\n", average);
+}
+
+// 整数提升
+void example_for_integer_imporve_transfer()
+{
+    int i = 17;
+    char c = 'c';
+    int sum = 0;
+    sum = i + c;
+    printf("the result is :%d\n", sum);
 }
 
 /*
@@ -54,6 +66,29 @@ void example_for_simple_transfer()
     printf("%.2f\n", average);
 }
 ```
+
+* 上方的例子中，强制转换的优先级大于算术运算符（`/`）的优先级
+* 类型转换可以是隐式的，由编译器自动执行，也可以是显式的，通过使用强制类型转换运算符来指定。
+* 在编程时，有需要类型转换的时候都用上强制类型转换运算符，是一种良好的编程习惯。
+
+### 整数提升
+* 整数提升是指把小于 int 或 unsigned int 的整数转换为 int 或 unsigned int 的过程。如下示例：
+
+```c
+// 整数提升
+void example_for_integer_imporve_transfer()
+{
+    int i = 17;
+    char c = 'c';
+    int sum = 0;
+    sum = i + c;
+    printf("the result is :%d\n", sum);
+}
+```
+
+* 运行后，得出的结果值是：116。可见，char 类型的变量 `c` 被提升为 int 类型（对应的 ASCII 值）。
+
+### 常用的算术转换
 
 
 */
